5f0bbfb33bed1d1e02f28364685e874094985553
zypher365 <ashimrajkonwar@gmail.com>
2017-03-19 17:21:23 +0530

    Serial communication

---
+++ DAY 2/Wifi.c
@@ -0,0 +1,31 @@
+/*
+ * Wifi.c
+ *
+ *  Created on: 19-Mar-2017
+ *      Author: Ashim
+ */
+
+
+
+#include <msp430f5529.h>
+
+
+int main(void)
+{
+
+}
+
+// Echo back RXed character, confirm TX buffer is ready first
+
+#pragma vector=USCI_A1_VECTOR
+
+__interrupt void USCI_A1_ISR(void)
+{
+
+}
+
+
+
+
+
+
